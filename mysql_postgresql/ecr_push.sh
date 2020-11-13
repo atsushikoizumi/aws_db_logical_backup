7878b151bff884777655647ba78c627cf0a740af
koizumi-dev <atsushi.koizumi@aqua-systems.co.jp>
2020-11-13 22:40:37 +0900

    mysql 追加

--- mysql_postgresql/ecr_push.sh
+++ mysql_postgresql/ecr_push.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+#
+# 自身のURLを指定してください。
+#
+ECR_URI=532973931974.dkr.ecr.eu-north-1.amazonaws.com/koizumi-dev-logicalbackup-mypg
+
+
 echo -n "IMAGE_TAG: "
 read IMAGE_TAG
 
@@ -9,7 +15,6 @@ if [ -z "${IMAGE_TAG}" ]; then
 fi
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
-ECR_URI=532973931974.dkr.ecr.eu-north-1.amazonaws.com/koizumi-dev-logicalbackup
 
 docker build -t ${ECR_URI}:${IMAGE_TAG} ${SCRIPT_DIR}
 aws ecr get-login-password | docker login --username AWS --password-stdin ${ECR_URI%/*}
