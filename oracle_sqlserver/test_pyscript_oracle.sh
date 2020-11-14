e81fd069a11fd58dc3342cc61b686c35d60d901e
koizumi-dev <atsushi.koizumi@aqua-systems.co.jp>
2020-11-15 00:05:49 +0900

    develope

--- oracle_sqlserver/test_pyscript_oracle.sh
+++ oracle_sqlserver/test_pyscript_oracle.sh
@@ -4,6 +4,7 @@ export tags_env=dev
 export tags_owner=koizumi
 export DB_INSTANCE_IDENTIFIER=koizumi-dev-db-oracle-1st
 export DB_NAME=MASTERDB
+export DB_SCHEMAS='XA_ADM XB_ADM'
 export DB_MASTER=masteruser
 export PASSWORD_KEY=postgresql
 export DB_INSTANCE_CLASS=db.t3.medium
